c6fb7d145279f9c271c08548a54cc1f486b94b3d
IMRAN AHMED <project.developer.tech@gmail.com>
2026-07-23 14:55:28 +0600

    Update location.js

--- js/location.js
+++ js/location.js
@@ -16,8 +16,8 @@ RJF.renderLocation = function(){
       '<div class="loc-grid">' +
         '<div class="loc-card">' +
           '<div class="loc-row">' + RJF.iconSvg('pin') + '<div><h4>ঠিকানা</h4><span>' + l.address + '</span></div></div>' +
-          '<div class="loc-row">' + RJF.iconSvg('phone') + '<div><h4>ফোন</h4><span>' + l.phone + '</span></div></div>' +
-          '<div class="loc-row">' + RJF.iconSvg('mail') + '<div><h4>ইমেইল</h4><span>' + l.email + '</span></div></div>' +
+          '<div class="loc-row"><a href="tel:' + l.phone + '" class="loc-link">' + RJF.iconSvg('phone') + '<div><h4>ফোন</h4><span>' + l.phone + '</span></div></a></div>' +
+          '<div class="loc-row"><a href="mailto:' + l.email + '?subject=' + encodeURIComponent('যোগাযোগ - ' + RJF.data.brand.name) + '&body=' + encodeURIComponent('আসসালামু আলাইকুম,\n\nআমি আপনাদের সাথে যোগাযোগ করতে চাচ্ছি।\n\nবিষয়: \n\nধন্যবাদান্তে,\n') + '" class="loc-link">' + RJF.iconSvg('mail') + '<div><h4>ইমেইল</h4><span>' + l.email + '</span></div></a></div>' +
         '</div>' +
         '<div class="map-wrap"><iframe loading="lazy" referrerpolicy="no-referrer-when-downgrade" src="' + l.mapEmbed + '"></iframe></div>' +
       '</div>' +
